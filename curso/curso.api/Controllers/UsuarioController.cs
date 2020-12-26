72ac8238b90c18426cc69e6cda18461ba240b2d1
Marcilio <marciliorsilva93@gmail.com>
2020-12-26 00:50:30 -0300

    customizando erros

--- curso/curso.api/Controllers/UsuarioController.cs
+++ curso/curso.api/Controllers/UsuarioController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using curso.api.Filters;
 using curso.api.Model;
 using curso.api.Model.Usuarios;
 using Microsoft.AspNetCore.Http;
@@ -20,13 +21,19 @@ namespace curso.api.Controllers
         [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
         [HttpPost]
         [Route("logar")]
+        [ValidacaoModelStateCustomizado]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
+           // if (!ModelState.IsValid)
+           //{
+           //     return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+           // }
             return Ok(loginViewModelInput);
         }
 
         [HttpPost]
         [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
         public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
         {
             return Created("", registroViewModelInput);
